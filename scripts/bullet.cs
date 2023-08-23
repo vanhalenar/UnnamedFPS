c34d4d7bf241bb41e628bd259530e465497993f7
Timotej Halenár <xhalen00@vutbr.cz>
2023-08-23 19:19:45 +0200

    added skybox, changed bullet type to Node3D

--- scripts/bullet.cs
+++ scripts/bullet.cs
@@ -1,12 +1,14 @@
 using Godot;
 using System;
 
-public partial class bullet : RigidBody3D
+public partial class bullet : Node3D
 {
 	public bool shoot = false;
 
-	public const float speed = 10.0f;
-	public const float damage = 50.0f;
+	public const float speed = 100.0f;
+	public const float damage = 10.0f;
+
+	Vector3 zeroVector = new Vector3(0, 0, -speed);
 
 	private Area3D area3D { get; set; }
 	// Called when the node enters the scene tree for the first time.
@@ -16,7 +18,7 @@ public partial class bullet : RigidBody3D
 
 		area3D = (Area3D)GetNode("Area3D");
 		area3D.AreaEntered += (area) => _On_Area_body_Entered(area);
-		BodyEntered += (body) => _On_Physics_body_Entered(body);
+		//BodyEntered += (body) => _On_Physics_body_Entered(body);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,11 +27,21 @@ public partial class bullet : RigidBody3D
 		if (shoot == true)
 		{
 			//ApplyImpulse(Transform.Basis.Z, -Transform.Basis.Z * speed);
-			ApplyCentralImpulse(-Transform.Basis.Z * speed);
+			//ApplyCentralImpulse(-Transform.Basis.Z * speed);
+			
 		}
 	}
 
-	private void _On_Physics_body_Entered(Node body)
+    public override void _Process(double delta)
+	{
+		if (shoot == true)
+		{
+			Position += Transform.Basis * zeroVector * (float)delta;
+		}
+        
+    }
+
+    private void _On_Physics_body_Entered(Node body)
 	{
 		GD.Print("hit here");
 		QueueFree();
